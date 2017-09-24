e4a02cc45098eb25739ba906187ba878d104acc5
Guilherme Moreira <guilhermegms.silva@gmail.com>
2017-09-24 14:18:44 -0300

    Correção do método que executava e somava um ao click no começo

--- js/app.js
+++ js/app.js
@@ -1,16 +1,21 @@
-var Controller = function(){
+var Controller = function() {
     this.image = ko.observable("img/cat.jpeg");
     this.catName = ko.observable("Josh");
     this.timesClicked = ko.observable(0);
-    this.incrementClick = function(){
-        this.timesClicked(this.timesClicked() +1);
+    this.incrementClick = function() {
+        this.timesClicked(this.timesClicked() + 1);
     };
-    this.catLevel = function(){
+    this.nicknames = [
+        "Toddy",
+        "Charlie",
+        "Thomas",
+    ];
+    this.catLevel = function() {
         var clicks = this.timesClicked();
-        if(clicks<50) return "Novinho";
-        else if(clicks<100) return "Intermediário";
-        else return "Fodelão";
-    }
+        if (clicks < 50) return "Novinho";
+        else if (clicks < 100) return "Intermediário";
+        else return "Avançado";
+    };
 };
 
 ko.applyBindings(new Controller());
