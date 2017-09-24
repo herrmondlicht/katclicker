c180c212729a843552c3ee47daae1581209e44c9
Guilherme Moreira <guilhermegms.silva@gmail.com>
2017-09-24 17:00:03 -0300

    parte do knockout completa

--- js/app.js
+++ js/app.js
@@ -1,20 +1,36 @@
-var Controller = function() {
-    this.image = ko.observable("img/cat.jpeg");
-    this.catName = ko.observable("Josh");
-    this.timesClicked = ko.observable(0);
-    this.incrementClick = function() {
-        this.timesClicked(this.timesClicked() + 1);
-    };
-    this.nicknames = [
-        "Toddy",
-        "Charlie",
-        "Thomas",
-    ];
-    this.catLevel = function() {
+var cats = [];
+if (localStorage.cats) {
+    cats = JSON.parse(localStorage.cats);
+} else {
+    localStorage.cats = JSON.stringify([]);
+}
+
+var Cat = function(data) {
+    this.image = ko.observable(data.path);
+    this.catName = ko.observable(data.name);
+    this.timesClicked = ko.observable(data.timesClicked);
+    this.nicknames = ko.observableArray(data.nicknames);
+    this.catLevel = ko.computed(function() {
         var clicks = this.timesClicked();
-        if (clicks < 50) return "Novinho";
+        if (clicks < 50) return "Noob";
         else if (clicks < 100) return "Intermediário";
         else return "Avançado";
+    }, this);
+
+
+};
+
+var Controller = function() {
+    var self = this;
+
+    this.currentCat = ko.observable(new Cat({
+        path: "img/cat.jpeg",
+        name: "Josh",
+        timesClicked: 0,
+        nicknames: ["Jojo"]
+    }));
+    this.incrementClick = function() {
+        self.currentCat().timesClicked(self.currentCat().timesClicked() + 1);
     };
 };
 
