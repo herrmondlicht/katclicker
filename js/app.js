b422afacb440cc2db217ee341a3ccc07f9d3ba10
Guilherme Moreira <guilhermegms.silva@gmail.com>
2017-09-24 23:59:34 -0300

    Criação de múltiplos objetos e implementação do observableArray

--- js/app.js
+++ js/app.js
@@ -1,12 +1,13 @@
-var cats = [];
-if (localStorage.cats) {
-    cats = JSON.parse(localStorage.cats);
-} else {
-    localStorage.cats = JSON.stringify([]);
-}
+//array de gatos
+var cats = [
+    {image:"img/cat.jpeg",name:"Gerônimo",nicknames:["Gê","Mimo"],timesClicked:0},
+    {image:"img/cat2.jpeg",name:"Arlindo",nicknames:["Lê","Arizinho"],timesClicked:0},
+    {image:"img/cat3.jpeg",name:"Preguiça",nicknames:["Levinho","Etc"],timesClicked:0}
+];
 
+//objeto que cria um gato à partir dos dados passados
 var Cat = function(data) {
-    this.image = ko.observable(data.path);
+    this.image = ko.observable(data.image);
     this.catName = ko.observable(data.name);
     this.timesClicked = ko.observable(data.timesClicked);
     this.nicknames = ko.observableArray(data.nicknames);
@@ -16,22 +17,21 @@ var Cat = function(data) {
         else if (clicks < 100) return "Intermediário";
         else return "Avançado";
     }, this);
-
-
 };
 
 var Controller = function() {
     var self = this;
-
-    this.currentCat = ko.observable(new Cat({
-        path: "img/cat.jpeg",
-        name: "Josh",
-        timesClicked: 0,
-        nicknames: ["Jojo"]
-    }));
+    this.catList = ko.observableArray([]);
+    cats.forEach(function(obj){
+        self.catList.push(new Cat(obj));
+    });
+    this.currentCat = ko.observable(this.catList()[0]);
     this.incrementClick = function() {
         self.currentCat().timesClicked(self.currentCat().timesClicked() + 1);
     };
+    this.setCurrentCat = function(){
+        self.currentCat(this);
+    }
 };
 
 ko.applyBindings(new Controller());
